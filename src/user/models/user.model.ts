78550a7ef64cf06ca33f1cdb2c745c2bd4198ce5
roytiger81102 <roytiger81102@gmail.com>
2024-09-24 13:13:15 +0900

    feat: add User create and get functions

---
+++ src/user/models/user.model.ts
@@ -0,0 +1,22 @@
+import { Field, Int, HideField, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class User {
+  @Field(() => Int)
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  email: string;
+
+  @HideField()
+  password: string;
+
+  @Field()
+  createdAt: Date;
+
+  @Field()
+  updatedAt: Date;
+}
